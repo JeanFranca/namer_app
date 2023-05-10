88ea8607b08632219432549708116905b6fac3c5
Jean França <jeanfranca8992@gmail.com>
2023-05-09 23:17:59 -0300

    adição da exibição de palavras favoritas

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError("no widget for $selectedIndex");
@@ -103,6 +103,38 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         );
       }
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    final theme = Theme.of(context);
+    
+    if(appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+    
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have ${appState.favorites.length} favorites: '),
+        ),
+        for(var word in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(word.asLowerCase),
+          )
+      ]
     );
   }
 }
